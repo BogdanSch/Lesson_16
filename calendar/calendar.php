7f2675cc76d95ef94dd0048e8e0e6a56c22d4085
Bohdan <bogmira777@gmail.com>
2022-12-02 22:24:50 +0100

    Add calendar task

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -9,19 +9,35 @@
 </head>
 <body>
 <?php
-$month_start = strtotime("1.11.2022");
 
-$month_days = 30;
-$month_weeks_count = floor(30 / 7);
-$day_start = 1;
+$month_days = 32;
+$month_weeks_count = floor($month_days / 7);
+$last_day = 30;
+$next_day = 6;
 
 echo "<table><tr><th>Monday</th><th>Tuesday</th><th>Wendnesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th><th>Sunday</th></tr>";
-for ($i=1; $i <= $month_weeks_count; $i++) {
+echo "<tr><td>30</td><td>31</td>";
+
+for ($i=1; $i <= 5; $i++) { 
+    if($i == 5)
+        echo "<td style=\"background: red;\">$i</td>";
+    else
+        echo "<td>$i</td>";
+}
+echo "</tr>";
+
+for ($i=0; $i < $month_weeks_count; $i++) {
     echo "<tr>";
-    for ($y=$day_start; $y <= $day_start + 7; $y++) { 
-        echo "<td>$y<td>";
+    for ($y=$next_day; $y < $next_day + 7; $y++) {
+        if($y == $next_day + 6){
+            echo "<td style=\"background: red;\">$y</td>";
+        }
+        else{
+            echo "<td>$y</td>";
+        }
+        if($y >= $last_day) break;
     }
-    $day_start += 7;
+    $next_day += 7;
     echo "</tr>";
 }?>
 </body>
